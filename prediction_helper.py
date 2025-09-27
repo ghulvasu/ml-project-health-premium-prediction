bdb6568748173d03a8a26b1cf6b8aab4e36ca07e
ghulvasu <ghulvasu069@gmail.com>
2025-09-27 23:09:03 +0530

    File path updated

--- prediction_helper.py
+++ prediction_helper.py
@@ -4,10 +4,10 @@ import pandas as pd
 import joblib
 
 # Corrected file paths (use raw strings to avoid escape sequence issues)
-model_young = joblib.load(r"ReadyModels\model_young.joblib")
-model_rest = joblib.load(r"ReadyModels\model_rest.joblib")
-scaler_young = joblib.load(r"ReadyModels\scaler_young.joblib")
-scaler_rest = joblib.load(r"ReadyModels\scaler_rest.joblib")
+model_young = joblib.load(r"ReadyModels/model_young.joblib")
+model_rest = joblib.load(r"ReadyModels/model_rest.joblib")
+scaler_young = joblib.load(r"ReadyModels/scaler_young.joblib")
+scaler_rest = joblib.load(r"ReadyModels/scaler_rest.joblib")
 
 
 def calculate_normalized_risk(medical_history):
